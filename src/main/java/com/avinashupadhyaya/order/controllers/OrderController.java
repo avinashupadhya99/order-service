eb64ff8cfc97f69036cdb0ba5c5621dc63d8413f
avinashupadhya99 <avinashupadhya99@gmail.com>
2022-04-02 08:14:17 +0000

    feat: get orders by id

--- src/main/java/com/avinashupadhyaya/order/controllers/OrderController.java
+++ src/main/java/com/avinashupadhyaya/order/controllers/OrderController.java
@@ -1,5 +1,7 @@
 package com.avinashupadhyaya.order.controllers;
 
+import java.util.Optional;
+
 import com.avinashupadhyaya.order.model.Order;
 import com.avinashupadhyaya.order.repository.OrderRepository;
 
@@ -9,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,9 +26,27 @@ public class OrderController {
 
     private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
 
+    @RequestMapping(value = "/{orderID}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Order> getAllRoadmaps(@PathVariable("orderID") String orderID) {
+        try {
+            Optional<Order> order = orderRepository.findById(Long.parseLong(orderID));
+            if(!order.isPresent()) {
+                logger.error("No order");
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            logger.debug("Order {}", order.get().getOrderType());
+            return new ResponseEntity<>(order.get(), HttpStatus.OK);
+        } catch(Exception e) {
+            logger.error("Internal error {} ", e.getMessage());
+            e.printStackTrace();
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        
+    }
+
     @RequestMapping(value = "/new", 
             method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Order> createUser(@RequestBody Order order) {
+    public ResponseEntity<Order> createOrder(@RequestBody Order order) {
         Order newOrder = orderRepository.save(order);
         logger.info("New order created with OrderID - {}", newOrder.getId());
         return new ResponseEntity<>(newOrder, HttpStatus.OK);
