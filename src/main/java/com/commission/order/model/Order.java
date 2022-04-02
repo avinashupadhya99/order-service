4e78cebb8df9e1674e9b84688f2da4e4cd4ff8e2
avinashupadhya99 <avinashupadhya99@gmail.com>
2022-04-02 14:36:42 +0000

    feat: sequence generator for ID

--- src/main/java/com/commission/order/model/Order.java
+++ src/main/java/com/commission/order/model/Order.java
@@ -4,16 +4,26 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 
 @Entity
 @Table(name="commission_order")
 public class Order {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(
+      name = "sequence-generator",
+      strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+      parameters = {
+        @Parameter(name = "sequence_name", value = "order_sequence"),
+        @Parameter(name = "initial_value", value = "289756340"),
+        @Parameter(name = "increment_size", value = "1")
+        }
+    )
     private Long id;
     @NotBlank(message = "Order Type is mandatory")
     private String type;
