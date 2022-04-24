231f34c2ccc0e66613bd885c4724c1fcb828097d
avinashupadhya99 <avinashupadhya99@gmail.com>
2022-04-24 19:31:41 +0000

    chore: span tags

--- src/main/java/com/commission/order/controllers/OrderController.java
+++ src/main/java/com/commission/order/controllers/OrderController.java
@@ -21,6 +21,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import io.opentracing.Span;
+import io.opentracing.util.GlobalTracer;
 
 @RestController
 @RequestMapping("/api/v1/orders")
@@ -40,6 +42,10 @@ public class OrderController {
     @RequestMapping(value = "/{orderID}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Order> getOrder(@PathVariable("orderID") String orderID) {
         try {
+            final Span span = GlobalTracer.get().activeSpan();
+            if (span != null) {
+                span.setTag("order_id", orderID);
+            }
             Optional<Order> order = orderRepository.findById(Long.parseLong(orderID));
             if(!order.isPresent()) {
                 logger.error("No order");
@@ -61,6 +67,10 @@ public class OrderController {
         try {
             Order newOrder = orderRepository.save(order);
             logger.info("New order created with OrderID - {}", newOrder.getId());
+            final Span span = GlobalTracer.get().activeSpan();
+            if (span != null) {
+                span.setTag("order_id", newOrder.getId());
+            }
             produceService.createProduce(newOrder);
             return new ResponseEntity<>(newOrder, HttpStatus.OK);
         } catch(ConstraintViolationException ex) {
@@ -78,6 +88,10 @@ public class OrderController {
             method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Order> completeOrder(@RequestBody JsonNode completeOrder) {
         try {
+            final Span span = GlobalTracer.get().activeSpan();
+            if (span != null) {
+                span.setTag("order_id", completeOrder.get("orderId").asText("NA"));
+            }
             Optional<Order> order = orderRepository.findById(completeOrder.get("orderId").asLong());
             if(!order.isPresent()) {
                 logger.error("No order");
